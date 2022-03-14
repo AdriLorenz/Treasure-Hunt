584fb3f5a0829246c9098b612cacb32183fc7f01
PdgarHern <pdgarher@gmail.com>
2022-03-14 13:14:52 +0000

    react basic structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 // Routing
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Context
+import UserProvider from "./context";
+// Styles
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
   return (
-    <h1>Hola</h1>
+    <>
+      <Router>
+        <UserProvider>
+          <h1>Hola</h1>
+          <Routes></Routes>
+          <GlobalStyle />
+        </UserProvider>
+      </Router>
+    </>
+    
   );
 }
 
