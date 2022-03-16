00ea9f17bfd449d8fa4b224ba531a8ae25868c4e
Gnagna <sallgnagna71@gmail.com>
2022-03-16 10:36:40 +0100

    design

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,9 +2,9 @@ import React from "react";
 // Images
 import AppLogo from "../../assets/images/RSVamos_Logo.png";
 //Styles
-import { Wrapper, LogoImg, Content, Image , Email, Phone, Location, SocialsIcons} from "./Footer.styles";
+import { Wrapper, LogoImg, Content, Image, Email, Phone, Location, SocialsIcons } from "./Footer.styles";
 
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 // Images
 
 import LocationPageIcon from "../../assets/images/RSVamos_LocationPageIcon.svg";
@@ -14,35 +14,38 @@ import FacebookIcon from "../../assets/images/RSVamos_FacebookIcon.svg";
 import TwitterIcon from "../../assets/images/RSVamos_TwitterIcon.svg";
 import InstagramIcon from "../../assets/images/RSVamos_InstagramIcon.svg";
 
-const Footer = () =>{
-      return(
-          <Wrapper>
-               <Content>
-                   <LogoImg src={AppLogo} alt='app-logo'/>
-                   <Location>
-                        <img src={LocationPageIcon} alt="locationIcon" className="whiteIcon"/>
+const Footer = () => {
+    return (
+        <footer>
+            <section>
+                <img src={AppLogo} alt="logo" />
+            </section>
+            <section>
+                <ul>
+                    <li>
+                        <img src={LocationPageIcon} alt="locationIcon" className="whiteIcon" />
                         <p className="location"> Gran Canaria Street</p>
-                    </Location>
-                    <Email>
-                       <img src={EmailIcon} alt="emailIcon" className="emailIcon"/>
-                       <p className="email"> Vamos@hotmail.com</p>
-                    </Email>
-                    <Phone>
-                       <img src={PhoneIcon} alt="phoneIcon" className="phoneIcon"/> 
-                       <p className="phone"> +34 69 69 69 696</p>
-                    </Phone>
-                    <SocialsIcons>
-                        <Image src={FacebookIcon} alt="facebookIcon"/>
-                        <Image src={TwitterIcon} alt="twitterIcon"/>
-                        <Image src={InstagramIcon} alt="instagramIcon"/>  
-                    </SocialsIcons>
+                    </li>
+                    <li>
+                        <img src={EmailIcon} alt="emailIcon" className="emailIcon" />
+                        <p className="email"> Vamos@hotmail.com</p>
+                    </li>
+                    <li>
+                        <img src={PhoneIcon} alt="phoneIcon" className="phoneIcon" />
+                        <p className="phone"> +34 69 69 69 696</p>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <img src={InstagramIcon} alt="instagramIcon" />
+                <img src={FacebookIcon} alt="facebookIcon" />
+                <img src={TwitterIcon} alt="twitterIcon" />
+            </section>
 
 
+        </footer>
 
-               </Content>
-               
-          </Wrapper>
-      )
+    )
 }
 
 
